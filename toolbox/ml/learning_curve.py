26960583eb7ddb2ea86cb174a1c03b729c3ce0b1
Maggie Jakus <margaret.jakus@students.olin.edu>
2015-03-19 10:44:59 -0400

    Testing machine learning

--- toolbox/ml/learning_curve.py
+++ toolbox/ml/learning_curve.py
@@ -6,13 +6,21 @@ from sklearn.datasets import *
 from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import LogisticRegression
 
+digits = load_digits()
 data = load_digits()
 print data.DESCR
 num_trials = 10
 train_percentages = range(5,95,5)
+print len(train_percentages)
 test_accuracies = numpy.zeros(len(train_percentages))
+print len(test_accuracies)
+print type(test_accuracies)
 fig = plt.figure()
 for i in range(10):
+	subplot = fig.add_subplot(5,2,i+1)
+	subplot.matshow(numpy.reshape(digits.data[i],(8,8)),cmap='gray')
+
+plt.show	
 	
 
 # train a model with training percentages between 5 and 90 (see train_percentages) and evaluate
@@ -21,6 +29,20 @@ for i in range(10):
 # for consistency with the previous example use model = LogisticRegression(C=10**-10) for your learner
 
 # TODO: your code here
+
+
+for size in train_percentages:
+	total_accuracy = 0
+	for i in range(0, num_trials):
+		X_train, X_test, y_train, y_test = train_test_split(data.data, data.target, train_size = size)
+		model = LogisticRegression(C=10**-10)
+		model.fit(X_train,y_train)
+		total_accuracy += model.score(X_test, y_test)
+
+	test_accuracies[size/5.0-1] = total_accuracy/float(num_trials)
+	print size
+	print "Train accuracy %f" %model.score(X_train,y_train)
+	print "Test accuracy %f" %model.score(X_test, y_test)
 
 fig = plt.figure()
 plt.plot(train_percentages, test_accuracies)
